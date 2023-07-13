3e0ffb3c236c97bfb4d312cb67581aa38bc3942e
Sanjeeb Nepal <45708638+sanjeebnepal@users.noreply.github.com>
2023-07-13 14:45:51 +0545

    appstart modified

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
 cd /home/ec2-user/server/src
-npm start &
 
+# Start the application in the background
+npm start > /dev/null 2>&1 &
+
+# Wait for the application to start (adjust the sleep duration if needed)
+sleep 10
+
+# Start the application with PM2
 pm2 start npm --name "sanjeebapp" -- start
+
+# Configure PM2 to start on system boot
 pm2 startup
+
+# Save the PM2 process list
 pm2 save
-pm2 restart
+
+# Restart all PM2 processes (including the application)
+pm2 restart all
